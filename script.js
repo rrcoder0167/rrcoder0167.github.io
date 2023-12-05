e9f490e11c1f6212e84769d1069139cf8a4e87c4
rrcoder0167 <riddhiman.rana@gmail.com>
2023-12-04 20:47:16 -0800

    Add typewriter effect to profile description

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ function updateActiveLink() {
 
 // Call the function to update the active link when the page loads
 updateActiveLink();
+
+const words = ["Full Stack Developer", "An amazing person", "Compassionate"];
+        let wordIndex = 0;
+        let isDeleting = false;
+        let text = "";
+
+        function typeWriter() {
+            const current = words[wordIndex];
+            const endOfWord = text.length === current.length;
+            const deletedAll = text.length === 0;
+
+            if (endOfWord && !isDeleting) {
+                isDeleting = true;
+            } else if (deletedAll && isDeleting) {
+                isDeleting = false;
+                wordIndex = (wordIndex + 1) % words.length;
+            }
+
+            if (isDeleting) {
+                text = current.substring(0, text.length - 1);
+            } else {
+                text = current.substring(0, text.length + 1);
+            }
+
+            document.getElementById('text').innerText = text;
+        }
+
+        setInterval(typeWriter, 200);

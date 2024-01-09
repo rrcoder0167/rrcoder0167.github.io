170b888b6c4ba2b00e16df0d1d4aaf9f77d6e41b
Riddhiman Rana <riddhiman.rana@gmail.com>
2024-01-09 18:03:44 +0000

    update vercel w/ analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,48 @@
-import type { Metadata } from 'next'
-import { Rubik } from 'next/font/google'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import './globals.css'
-import Navbar from '@/components/Navbar'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 // Cloudflare Pages Edge Runtime
-export const runtime = 'edge';
+export const runtime = "edge";
 
-const rubik = Rubik({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Riddhiman Rana - Developer Portfolio',
-  description: 'My name is Riddhiman Rana and I am a full stack developer. Find out more about me and my projects.',
-}
+  title: "Riddhiman Rana - Developer Portfolio",
+  description:
+    "My name is Riddhiman Rana and I am a full stack developer. Find out more about me and my projects.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <>
-    <html lang="en">
-    <head>
-        <script defer data-domain="rrcoder0167.is-a.dev" src="https://analytics.is-a.dev/js/script.js"></script>
-        <link rel="stylesheet" href="https://unpkg.com/@catppuccin/palette/css/catppuccin.css" />
-    </head>
-    <body className={rubik.className}>
-    <Navbar />
-      {children}
-    </body>
-     </html>
-</>
-  )
+      <html lang="en">
+        <head>
+          <script
+            defer
+            data-domain="rrcoder0167.is-a.dev"
+            src="https://analytics.is-a.dev/js/script.js"
+          ></script>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/@catppuccin/palette/css/catppuccin.css"
+          />
+        </head>
+        <body className={rubik.className}>
+          <Navbar />
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </body>
+      </html>
+    </>
+  );
 }

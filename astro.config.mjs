f22f1576edb8b089535b39f6dcdafeb53fd9d641
Riddhiman Rana <riddhiman.rana@gmail.com>
2024-03-13 17:21:49 +0000

    Increase SEO perf w/robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  site: 'https://rrcoder0167.is-a.dev',
+  integrations: [tailwind(), icon(), react(), robotsTxt()]
 });

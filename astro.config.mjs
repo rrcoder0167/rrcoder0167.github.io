d2df8682011b515575113bbd7cfe3ca86bc58f40
rrcoder0167 <riddhiman.rana@gmail.com>
2024-03-15 11:13:44 -0700

    add typewriter animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import react from "@astrojs/react";
-
 import robotsTxt from "astro-robots-txt";
+
+import pageInsight from "astro-page-insight";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rrcoder0167.is-a.dev',
-  integrations: [tailwind({ nesting:true, }), icon(), react(), robotsTxt()]
+  integrations: [tailwind({
+    nesting: true
+  }), icon(), robotsTxt(), pageInsight()]
 });

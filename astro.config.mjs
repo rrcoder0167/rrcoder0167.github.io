1a000e115822c18ccbee1c9ed305cb3c5aab9e7c
rrcoder0167 <riddhiman.rana@gmail.com>
2024-03-15 18:25:10 -0700

    Add markdown rendering with syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,22 @@ import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import pageInsight from "astro-page-insight";
 
+
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    render: [
+      '@astrojs/markdown-remark',
+      {
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+          theme: 'nord',
+          langs: ['js', 'html', 'css', 'astro'],
+          wrap: false,
+        },
+      },
+    ],
+  },
   site: 'https://rrcoder0167.is-a.dev',
   integrations: [tailwind({
     nesting: true
